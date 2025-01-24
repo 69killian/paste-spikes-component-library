255eaae97316a0bd8ece780a13d881fdd9c1391a
Killian <martykillian7@gmail.com>
2025-01-24 11:47:24 +0100

    pushing to vercel

--- app/Components/button.tsx
+++ app/Components/button.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode;
-  className?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, className, onClick, ...props }) => {
