63061fe2ac43af29052c7b4f8bb6d434c24af282
Killian <martykillian7@gmail.com>
2025-01-26 11:14:08 +0100

    Hero section and responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
+import Content from "./Components/Content";
 
 export default function Home() {
   return (
     <>
+    
     <Header/>
     <Sidebar/>
-    <div>Hello World</div>
+    <div className="lg:pl-[320px] pt-[125px] pr-[40px]">
+    <Content/>
+    </div>
     </>
   );
 }
